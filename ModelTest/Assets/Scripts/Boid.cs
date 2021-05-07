366fcb9d848981e6f26372c31d7ac00c673260f0
PhilToolan <c17433026@mydit.ie>
2021-05-07 12:29:09 +0100

    changes to states

--- ModelTest/Assets/Scripts/Boid.cs
+++ ModelTest/Assets/Scripts/Boid.cs
@@ -19,15 +19,14 @@ public class Boid : MonoBehaviour
     public float maxSpeed = 5.0f;
     public float maxForce = 10.0f;
 
+    public GameObject player;
+
     public void OnDrawGizmos()
     {
-        Gizmos.color = Color.magenta;
+        Gizmos.color = Color.blue;
         Gizmos.DrawLine(transform.position, transform.position + velocity);
 
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(transform.position, transform.position + acceleration);
-
-        Gizmos.color = Color.red;
+        Gizmos.color = Color.yellow;
         Gizmos.DrawLine(transform.position, transform.position + force * 10);
     }
 
