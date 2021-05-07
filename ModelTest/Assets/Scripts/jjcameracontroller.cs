da6d4c76e249fbb052212b758aca44385b5e15e7
PhilToolan <c17433026@mydit.ie>
2021-05-07 14:29:25 +0100

    New camera controllers

---
+++ ModelTest/Assets/Scripts/jjcameracontroller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jjcameracontroller : MonoBehaviour
+{
+    public Camera camera;
+    public float focalLength = 50.0f;
+    private float lerp = 0f;
+    private float duration = 5f;
+    private float mid = 50f;
+    private float zoom = 100f;
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            lerp += Time.deltaTime / duration;
+            focalLength = Mathf.Lerp(mid, zoom, lerp);
+            camera.focalLength = focalLength;
+        }
+
+
+        transform.LookAt(target);
+
+        lerp += Time.deltaTime / duration;
+        focalLength = Mathf.Lerp(mid, zoom, lerp);
+        camera.focalLength = focalLength;
+    }
+}
