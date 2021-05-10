b12537a3b129f4448bffbe3ce9f0c9675ac16043
PhilToolan <c17433026@mydit.ie>
2021-05-10 13:01:17 +0100

    first 3 camera shots complete

---
+++ ModelTest/Assets/Scripts/ChangeCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+    public GameObject camera1;
+    public GameObject camera2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "MainCharacters")
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(true);
+        }
+        if (col.gameObject.tag == "ArrivalCamera")
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(true);
+        }
+    }
+}
