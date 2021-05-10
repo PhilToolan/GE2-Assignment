dd3c00f880451de13ebaed806676d0b17abb283a
PhilToolan <c17433026@mydit.ie>
2021-05-10 10:22:55 +0100

    opening scene and beam particle system

--- ModelTest/Assets/Scripts/OpeningCamera.cs
+++ ModelTest/Assets/Scripts/OpeningCamera.cs
@@ -7,6 +7,10 @@ public class OpeningCamera : MonoBehaviour
 
     public Transform target;
 
+
+    float smoothSpeed = 0.02f;
+    public Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,15 @@ public class OpeningCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target.parent);
     }
 }
