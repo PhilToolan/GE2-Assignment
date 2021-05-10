84574a040131b06ae4f7d4edd4bf65baa8d0ac97
PhilToolan <c17433026@mydit.ie>
2021-05-10 20:03:01 +0100

    ji

---
+++ ModelTest/Assets/Scripts/DisableBoid.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableBoid : MonoBehaviour
+{
+    public GameObject main;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "303")
+        {
+            main.GetComponent<Boid>().enabled = false;
+        }
+    }
+}
