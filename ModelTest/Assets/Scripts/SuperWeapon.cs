50291c7ea37b6ab4b59b0de481a61eeac1fb19f1
PhilToolan <c17433026@mydit.ie>
2021-05-10 20:11:52 +0100

    Super weapon

---
+++ ModelTest/Assets/Scripts/SuperWeapon.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperWeapon : MonoBehaviour
+{
+    public Transform target;
+    public GameObject bulletPrefab;
+    public bool shooting = true;
+    public int fireRate = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(Shoot());
+    }
+
+    IEnumerator Shoot()
+    {
+        while (true)
+        {
+            if (shooting)
+            {
+                GameObject bullet = GameObject.Instantiate<GameObject>(bulletPrefab
+                    , transform.position + transform.forward * 2
+                    , transform.rotation
+                    );
+
+            }
+            yield return new WaitForSeconds(1.0f / (float)fireRate);
+        }
+    }
+}
