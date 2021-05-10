9a6afbd1ec1f074d3d3f8254f017d2b2d1fa2339
PhilToolan <c17433026@mydit.ie>
2021-05-10 15:47:46 +0100

    camera shake

---
+++ ModelTest/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    // How long the object should shake for.
+    public float shakeDuration = 0f;
+
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount = 0.7f;
+    public float decreaseFactor = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = transform.position + Random.insideUnitSphere * shakeAmount;
+
+        shakeDuration -= Time.deltaTime * decreaseFactor;
+    }
+}
